36380a5f38d062846381d69e467b4c13fc891bbe
pangcl <1350306170@qq.com>
2026-06-26 15:30:52 +0800

    feat(database): 增强数据库连接配置功能
    - 添加密码版本选择的筛选和创建功能，并显示当前密码标签
    - 新增直接执行SQL的选项控制SQL执行方式
    - 将默认数据库主机地址从192.168.31.82改为127.0.0.1
    - 添加directExecution字段用于控制SQL执行模式
    - 实现密码标签的颜色区分和动态显示功能
    - 更新URL判断逻辑以支持不同的部署环境
    - 修改服务API地址从localhost改为192.168.3.227

--- js/_variable.js
+++ js/_variable.js
@@ -1,31 +1,31 @@
 var URLL = window.location.href;
 let R = '';
-if(URLL.indexOf('pangchunlei') > -1) {
+if (URLL.indexOf('pangchunlei') > -1) {
     R = 'http://pangchunlei.xyz/blog/php/'
-} else if(URLL.indexOf('abeiyun') > -1) {
+} else if (URLL.indexOf('abeiyun') > -1) {
     R = 'http://ftp6263399.host108.abeiyun.cn/blog/php/'
 } else {
     R = 'http://localhost:89/blog/php/'
 }
 const requestHttp = R
 
-const ajaxSavemysqlPhp = 'http://localhost:89/mywww/php/savemysql.php'
-const ajaxFileDealPhp = 'http://localhost:89/mywww/php/requestOnline.php'
-
-// 处理ajaxFileDealJavaIP：从当前URL提取IP/域名并拼接端口28019
+// 处理ajaxFileDealJavaIP：根据当前URL判断使用localhost还是其他环境的接口
 let ajaxFileDealJavaIP = '';
 try {
+    // 创建URL对象解析当前地址
+    const urlObj = new URL(URLL);
+    const hostname = urlObj.hostname;
+    const protocol = urlObj.protocol;
+
     // 检查是否为 pcl1350306170.github.io 域名
-    if (URLL.includes('pcl1350306170.github.io')) {
+    if (hostname === 'localhost' || hostname === '127.0.0.1') {
+        // 如果是localhost或127.0.0.1，使用localhost接口
+        ajaxFileDealJavaIP = 'http://localhost:28019';
+    } else if (URLL.includes('pcl1350306170.github.io')) {
         // 特殊处理：使用指定IP
         ajaxFileDealJavaIP = 'http://192.168.18.228:28019';
     } else {
-        // 创建URL对象解析当前地址
-        const urlObj = new URL(URLL);
-        // 提取协议（http:或https:）和主机名（包含IP或域名，不含端口）
-        const protocol = urlObj.protocol;
-        const hostname = urlObj.hostname;
-        // 拼接成 "协议//主机名:28019" 格式
+        // 其他情况：从当前URL提取IP/域名并拼接端口28019
         ajaxFileDealJavaIP = `${protocol}//${hostname}:28019`;
     }
 } catch (e) {
